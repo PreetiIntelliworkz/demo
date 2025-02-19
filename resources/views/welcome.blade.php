2eb207ec341071b905a312f24117023775b70c78
Preeti Singh <preeti@intelliworkz.tech>
2025-02-19 16:53:57 +0530

    change title4

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,6 +14,6 @@
        Hello World to All
     </head>
     <body class="font-sans antialiased dark:bg-black dark:text-white/50">
-       Hello World
+       Hello World To Laravel
     </body>
 </html>
